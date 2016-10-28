58417b9b3fcd794dc1060d19eb74a9898fe5d544
Viking Zhang <vikingmute@gmail.com>
2016-10-28 13:42:38 +0800

    change the port name

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ new WebpackDevServer(webpack(config), {
   publicPath: config.output.publicPath,
   hot: true,
   historyApiFallback: true
-}).listen(4000, 'localhost', function (err, result) {
+}).listen(3000, 'localhost', function (err, result) {
   if (err) {
     return console.log(err);
   }
